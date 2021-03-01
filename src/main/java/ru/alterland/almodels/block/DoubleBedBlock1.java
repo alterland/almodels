f7c8946b3d00d98213ac87cd0dd3dacc08b51a2c
Roman Sivtsov <romansivtsov340@gmail.com>
2021-03-02 00:41:30 +0400

    collisions done

--- src/main/java/ru/alterland/almodels/block/DoubleBedBlock1.java
+++ src/main/java/ru/alterland/almodels/block/DoubleBedBlock1.java
@@ -4,10 +4,13 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.block.AbstractBlock;;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.IBlockReader;
 import ru.alterland.almodels.tileentity.DoubleBedTileEntity1;
 
@@ -19,20 +22,20 @@ public class DoubleBedBlock1 extends IBigBlock {
 
     private static final VoxelShape[][] shape = {
         {
-                Block.makeCuboidShape(0,0,0, 15,15,15),
-                Block.makeCuboidShape(0,0,0, 15,15,15),
-                Block.makeCuboidShape(0,0,0, 15,15,15),
-                Block.makeCuboidShape(0,0,0, 15,15,15),
-                Block.makeCuboidShape(0,0,0, 15,15,15),
-                Block.makeCuboidShape(0,0,0, 15,15,15),
+                Block.makeCuboidShape(1,1,1, 15,4,15),
+                Block.makeCuboidShape(1,1,1, 16,16,16),
+                Block.makeCuboidShape(1,1,1, 16,16,16),
+                Block.makeCuboidShape(1,1,1, 16,15,16),
+                Block.makeCuboidShape(1,1,1, 16,16,16),
+                Block.makeCuboidShape(1,1,1, 16,16,16),
         },
         {
-                Block.makeCuboidShape(0,0,0, 15,15,15),
-                Block.makeCuboidShape(0,0,0, 15,15,15),
-                Block.makeCuboidShape(0,0,0, 15,15,15),
-                Block.makeCuboidShape(0,0,0, 15,15,15),
-                Block.makeCuboidShape(0,0,0, 15,15,15),
-                Block.makeCuboidShape(0,0,0, 15,15,15),
+                Block.makeCuboidShape(1,1,1, 16,16,16),
+                Block.makeCuboidShape(1,1,1, 16,16,16),
+                Block.makeCuboidShape(1,1,1, 16,16,16),
+                Block.makeCuboidShape(1,1,1, 16,15,16),
+                Block.makeCuboidShape(1,1,1, 16,16,16),
+                Block.makeCuboidShape(1,1,1, 16,16,16),
         }
     };
 
@@ -49,4 +52,22 @@ public class DoubleBedBlock1 extends IBigBlock {
     @Nullable
     @Override
     public TileEntity createNewTileEntity(IBlockReader worldIn) { return new DoubleBedTileEntity1(); }
+
+    public void onLanded(IBlockReader worldIn, Entity entityIn) {
+        if (entityIn.isSuppressingBounce()) {
+            super.onLanded(worldIn, entityIn);
+        } else {
+            this.bounceEntity(entityIn);
+        }
+
+    }
+
+    private void bounceEntity(Entity entity) {
+        Vector3d vector3d = entity.getMotion();
+        if (vector3d.y < 0.0D) {
+            double d0 = entity instanceof LivingEntity ? 1.0D : 0.8D;
+            entity.setMotion(vector3d.x, -vector3d.y * (double)0.66F * d0, vector3d.z);
+        }
+
+    }
 }
--- src/main/java/ru/alterland/almodels/block/IBigBlock.java
+++ src/main/java/ru/alterland/almodels/block/IBigBlock.java
@@ -1,9 +1,9 @@
 package ru.alterland.almodels.block;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.block.*;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.util.Direction;
@@ -12,7 +12,6 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-import org.apache.logging.log4j.Level;
 
 import javax.annotation.Nullable;
 
@@ -43,23 +42,42 @@ abstract public class IBigBlock extends HorizontalBlock implements ITileEntityPr
         }
     }
 
+    @Nullable
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        Direction direction = context.getPlacementHorizontalFacing();
+        BlockPos blockpos = context.getPos();
+        BlockPos blockpos1 = blockpos.offset(direction);
+        return context.getWorld().getBlockState(blockpos1).isReplaceable(context) ? this.getDefaultState().with(HORIZONTAL_FACING, direction) : null;
+    }
+
     public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
         super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
         if (!worldIn.isRemote) {
-            int i = 0;
 
             Direction direction = state.get(HORIZONTAL_FACING);
 
-            switch (direction) {
-                case NORTH:
-
-            }
+            int i = 0;
 
             for (int y = 0; y < HEIGHT; y++) {
                 for (int x = 0; x < LENGTH; x++) {
                     for(int z = 0; z < WIDTH; z++) {
-                        if (!pos.equals(new BlockPos(x,y,z))) {
-                            worldIn.setBlockState(pos.add(x,y,z), state.with(PART, i));
+                        BlockPos blockPos = pos;
+                        switch (direction) {
+                            case SOUTH:
+                                blockPos = pos.add(z,y,x);
+                                break;
+                            case WEST:
+                                blockPos = pos.add(-x,y,z);
+                                break;
+                            case NORTH:
+                                blockPos = pos.add(-z,y,-x);
+                                break;
+                            case EAST:
+                                blockPos = pos.add(x,y,-z);
+                                break;
+                        }
+                        if (!pos.equals(blockPos)) {
+                            worldIn.setBlockState(blockPos, state.with(PART, i));
                         }
                         i++;
                     }
@@ -69,16 +87,71 @@ abstract public class IBigBlock extends HorizontalBlock implements ITileEntityPr
     }
 
     public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
+        float shapeSize = LENGTH*WIDTH*HEIGHT;
         if (!worldIn.isRemote && player.isCreative()) {
 
             int part = state.get(PART);
+            Direction direction = state.get(HORIZONTAL_FACING);
+
+            BlockPos mainBlockPos = pos;
 
             if (part != 0) {
-                BlockPos blockpos = pos.offset(state.get(HORIZONTAL_FACING));
-                BlockState blockstate = worldIn.getBlockState(blockpos);
-                worldIn.setBlockState(blockpos, Blocks.AIR.getDefaultState(), 35);
-                worldIn.playEvent(player, 2001, blockpos, Block.getStateId(blockstate));
+                //find main block
+
+                while (part/shapeSize >= 0.5) { //make it 2d
+                    mainBlockPos = mainBlockPos.down();
+                    part = part-(LENGTH*WIDTH);
+                }
+
+                int col = part/WIDTH;
+                int row = part-(col*WIDTH);
+
+                switch (direction) {
+                    case SOUTH:
+                        mainBlockPos = mainBlockPos.offset(Direction.WEST, row);
+                        mainBlockPos = mainBlockPos.offset(Direction.NORTH, col);
+                        break;
+                    case WEST:
+                        mainBlockPos = mainBlockPos.offset(Direction.NORTH, row);
+                        mainBlockPos = mainBlockPos.offset(Direction.EAST, col);
+                        break;
+                    case NORTH:
+                        mainBlockPos = mainBlockPos.offset(Direction.EAST, row);
+                        mainBlockPos = mainBlockPos.offset(Direction.SOUTH, col);
+                        break;
+                    case EAST:
+                        mainBlockPos = mainBlockPos.offset(Direction.SOUTH, row);
+                        mainBlockPos = mainBlockPos.offset(Direction.WEST, col);
+                        break;
+                }
             }
+
+            BlockPos currentBlockPos = mainBlockPos;
+
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < LENGTH; x++) {
+                    for (int z = 0; z < WIDTH; z++) {
+                        switch (direction) {
+                            case SOUTH:
+                                currentBlockPos = mainBlockPos.add(z,y,x);
+                                break;
+                            case WEST:
+                                currentBlockPos = mainBlockPos.add(-x,y,z);
+                                break;
+                            case NORTH:
+                                currentBlockPos = mainBlockPos.add(-z,y,-x);
+                                break;
+                            case EAST:
+                                currentBlockPos = mainBlockPos.add(x,y,-z);
+                                break;
+                        }
+                        BlockState blockstate = worldIn.getBlockState(currentBlockPos);
+                        worldIn.setBlockState(currentBlockPos, Blocks.AIR.getDefaultState(), 35);
+                        worldIn.playEvent(player, 2001, currentBlockPos, Block.getStateId(blockstate));
+                    }
+                }
+            }
+
         }
 
         super.onBlockHarvested(worldIn, pos, state, player);
